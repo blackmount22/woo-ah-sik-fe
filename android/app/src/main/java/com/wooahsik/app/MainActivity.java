551964306e4b77c13dd46ad255cadf4af80faeb3
문경민 <kmmun@com2us.com>
2026-03-12 11:45:14 +0900

    android app 기능 추가 및 fav.icon 재 변경

---
+++ android/app/src/main/java/com/wooahsik/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.wooahsik.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.wooahsik.app',
   appName: 'wooahsik',
-  webDir: 'public'
+  webDir: 'out'
 };
 
 export default config;
